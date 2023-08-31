c6704e57e3be8f4ec5ba9541f14da9aac6342d52
Ayoub RABIYA <rabiyaeayoub@gmail.com>
2023-08-31 10:31:56 +0100

    fix 404 page style

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export default function NotFound() {
     return (
-        <div className="not-found-container">
+        <div className="not-found-container container">
             <h1>Sorry, the page you were looking for was not found.</h1>
             <Link to="/" className="link-button">Return to Home</Link>
         </div>
